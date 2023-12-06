081945945c717d4e63795fa12610f22c7ff05dc3
liushuang <liushuangbill@outlook.com>
2023-12-06 17:07:01 +0800

    fix: BytesToString

--- pkg/xstrings/xstrings.go
+++ pkg/xstrings/xstrings.go
@@ -13,5 +13,8 @@ func StringToBytes(s string) []byte {
 // BytesToString converts byte slice to string without a memory allocation.
 // For more details, see https://github.com/golang/go/issues/53003#issuecomment-1140276077.
 func BytesToString(b []byte) string {
+	if len(b) == 0 {
+		return ""
+	}
 	return unsafe.String(&b[0], len(b))
 }
