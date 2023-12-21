791e95446320c615d30ac8e902f9e2b3614d3ba0
liushuang <liushuangbill@outlook.com>
2023-12-21 11:04:25 +0800

    refactor: cors middleware

--- internal/routes/middleware/cors.go
+++ internal/routes/middleware/cors.go
@@ -1,15 +1,27 @@
 package middleware
 
 import (
+	"time"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Cors(allowAll bool, allowOrigins ...string) gin.HandlerFunc {
-	config := cors.DefaultConfig()
+	var config cors.Config
 	if allowAll {
-		config.AllowAllOrigins = true
+		config = cors.Config{
+			AllowMethods:           []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"},
+			AllowHeaders:           []string{"*"},
+			AllowCredentials:       false,
+			MaxAge:                 12 * time.Hour,
+			AllowWebSockets:        true,
+			AllowWildcard:          true,
+			AllowBrowserExtensions: true,
+			AllowAllOrigins:        true,
+		}
 	} else {
+		config = cors.DefaultConfig()
 		config.AllowOrigins = allowOrigins
 	}
 	return cors.New(config)
